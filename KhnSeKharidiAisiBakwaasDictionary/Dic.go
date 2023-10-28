00538930b31432ca8803d92a7d4986252576debe
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-10-29 03:27:10 +0530

    Added Update functionality, tested for existing key and non-existing key

--- KhnSeKharidiAisiBakwaasDictionary/Dic.go
+++ KhnSeKharidiAisiBakwaasDictionary/Dic.go
@@ -1,10 +1,18 @@
 package khnsekharidiaisibakwaasdictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrSearchWordNotFound = errors.New("search word not found in Dictionary")
+const (
+	ErrSearchWordNotFound         = DictionaryErr("search word not found in Dictionary")
+	ErrKeyAlreadyExist            = DictionaryErr("key already exists")
+	ErrCannotUpdateNonExistingKey = DictionaryErr("cannot update non existing key")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (dictionary Dictionary) Search(searchWord string) (string, error) {
 	result, ok := dictionary[searchWord]
@@ -12,4 +20,31 @@ func (dictionary Dictionary) Search(searchWord string) (string, error) {
 		return result, ErrSearchWordNotFound
 	}
 	return result, nil
+}
+
+func (dic Dictionary) Add(key, value string) error {
+	_, err := dic.Search(key)
+
+	switch err {
+	case ErrSearchWordNotFound:
+		dic[key] = value
+	case nil:
+		return ErrKeyAlreadyExist
+	default:
+		return err
+	}
+	return nil
+}
+
+func (dic Dictionary) Update(key, value string) error {
+	_, err := dic.Search(key)
+	switch err {
+	case nil:
+		dic[key] = value
+	case ErrSearchWordNotFound:
+		return ErrCannotUpdateNonExistingKey
+	default:
+		return err
+	}
+	return nil
 }
--- KhnSeKharidiAisiBakwaasDictionary/Dic_test.go
+++ KhnSeKharidiAisiBakwaasDictionary/Dic_test.go
@@ -3,31 +3,6 @@ package khnsekharidiaisibakwaasdictionary
 import "testing"
 
 func TestDic(t *testing.T) {
-
-	assertHelper := func(t testing.TB, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("Got '%s' want '%s'", got, want)
-		}
-	}
-
-	assertNoError := func(t testing.TB, got error) {
-		t.Helper()
-		if got != nil {
-			t.Fatal("Did not expect an error here")
-		}
-	}
-
-	assertError := func(t testing.TB, got, want error) {
-		t.Helper()
-		if got != want {
-			t.Errorf("Got '%s' want '%s'", got.Error(), want.Error())
-		}
-		if got == nil {
-			t.Fatal("Expected an error")
-		}
-	}
-
 	t.Run("Test Dic", func(t *testing.T) {
 		dictionary := Dictionary{"Hello": "It is a greeting"}
 		got, err := dictionary.Search("Hello")
@@ -42,4 +17,70 @@ func TestDic(t *testing.T) {
 		assertError(t, err, ErrSearchWordNotFound)
 	})
 
+	t.Run("Test add words", func(t *testing.T) {
+		dic := Dictionary{"Hello": "Greeting"}
+		key := "Test"
+		value := "This is a test"
+		addErr := dic.Add(key, value)
+		assertNoError(t, addErr)
+		got, err := dic.Search(key)
+		assertNoError(t, err)
+		assertHelper(t, got, value)
+	})
+
+	t.Run("Test add word exists", func(t *testing.T) {
+		pvalue := "Greeting"
+		key := "Hello"
+		value := "Again??"
+		dic := Dictionary{key: pvalue}
+		addErr := dic.Add(key, value)
+		assertError(t, addErr, ErrKeyAlreadyExist)
+		got, err := dic.Search(key)
+		assertNoError(t, err)
+		assertHelper(t, got, pvalue)
+
+	})
+
+}
+
+var assertHelper = func(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Got '%s' want '%s'", got, want)
+	}
+}
+
+var assertNoError = func(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("Did not expect an error here")
+	}
+}
+
+var assertError = func(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Got '%q' want '%q'", got, want)
+	}
+	if got == nil {
+		t.Fatal("Expected an error")
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("test update word", func(t *testing.T) {
+		dic := Dictionary{"Hello": "Greeting"}
+		upderr := dic.Update("Hello", "Updated Greeting")
+		assertNoError(t, upderr)
+		got, err := dic.Search("Hello")
+		assertNoError(t, err)
+		assertHelper(t, got, "Updated Greeting")
+	})
+	t.Run("test update word missing", func(t *testing.T) {
+		dic := Dictionary{"Hello": "Greeting"}
+		upderr := dic.Update("Test", "This is Test")
+		assertError(t, upderr, ErrCannotUpdateNonExistingKey)
+		_, err := dic.Search("Test")
+		assertError(t, err, ErrSearchWordNotFound)
+	})
 }
