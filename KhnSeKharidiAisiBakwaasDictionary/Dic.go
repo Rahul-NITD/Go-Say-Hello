7c6dd125f84c59fb6e839f24aca94ac58c995311
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-10-29 00:25:53 +0530

    Implemented Dictionary to handle known words and unknown words

--- KhnSeKharidiAisiBakwaasDictionary/Dic.go
+++ KhnSeKharidiAisiBakwaasDictionary/Dic.go
@@ -1,1 +1,13 @@
 package khnsekharidiaisibakwaasdictionary
+
+import "errors"
+
+type Dictionary map[string]string
+
+func (dictionary Dictionary) Search(searchWord string) (string, error) {
+	result, ok := dictionary[searchWord]
+	if !ok {
+		return result, errors.New("search word not found in Dictionary")
+	}
+	return result, nil
+}
--- KhnSeKharidiAisiBakwaasDictionary/Dic_test.go
+++ KhnSeKharidiAisiBakwaasDictionary/Dic_test.go
@@ -1,1 +1,34 @@
 package khnsekharidiaisibakwaasdictionary
+
+import "testing"
+
+func TestDic(t *testing.T) {
+
+	assertHelper := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got '%s' want '%s'", got, want)
+		}
+	}
+
+	t.Run("Test Dic", func(t *testing.T) {
+		dictionary := Dictionary{"Hello": "It is a greeting"}
+		got, err := dictionary.Search("Hello")
+		want := dictionary["Hello"]
+		if err != nil {
+			t.Fatal("Did not expect an error here")
+		}
+		assertHelper(t, got, want)
+	})
+
+	t.Run("Unknown Error", func(t *testing.T) {
+		dic := Dictionary{"Hello": "Greeting"}
+		_, err := dic.Search("Test")
+		want := "search word not found in Dictionary"
+		if err == nil {
+			t.Fatal("Expected an Error here")
+		}
+		assertHelper(t, err.Error(), want)
+	})
+
+}
