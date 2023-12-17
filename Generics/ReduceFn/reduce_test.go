26ce406530e11b24aa7490ab0954700c95856a95
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-17 10:47:36 +0000

    added init code for reduce

--- Generics/ReduceFn/reduce_test.go
+++ Generics/ReduceFn/reduce_test.go
@@ -1,1 +1,18 @@
 package reducefn_test
+
+import (
+	reducefn "GoSayHello/Generics/ReduceFn"
+	"testing"
+)
+
+func AssertEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v != %v", got, want)
+	}
+}
+
+func TestSum(t *testing.T) {
+	AssertEqual(t, reducefn.Sum([]int{1, 2, 3, 4, 5}), 15)
+	AssertEqual(t, reducefn.SumAllTails([]int{1, 2, 3, 4, 5})[0], 14)
+}
