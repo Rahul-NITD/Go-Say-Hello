fa6d3ae0db7520ce84dc8567168fef5935b0ddff
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-17 11:45:41 +0000

    Update reduce to return a different type after reducing

--- Generics/ReduceFn/reduce.go
+++ Generics/ReduceFn/reduce.go
@@ -4,14 +4,14 @@ package reducefn
 
 // Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
-	return Reduce[int](numbers, func(acc, i2 int) int {
+	return Reduce(numbers, func(acc, i2 int) int {
 		return acc + i2
 	}, 0)
 }
 
 // SumAllTails calculates the sums of all but the first number given a collection of slices.
 func SumAllTails(numbersToSum ...[]int) []int {
-	return Reduce[[]int](numbersToSum, func(acc, i2 []int) []int {
+	return Reduce(numbersToSum, func(acc, i2 []int) []int {
 		if len(i2) == 0 {
 			return append(acc, 0)
 		} else {
@@ -23,7 +23,7 @@ func SumAllTails(numbersToSum ...[]int) []int {
 // Again use generics to DRY up our code
 // create a reduce function
 
-func Reduce[T any](coll []T, accum func(T, T) T, initVal T) T {
+func Reduce[T, V any](coll []T, accum func(V, T) V, initVal V) V {
 	res := initVal
 	for _, val := range coll {
 		res = accum(res, val)
