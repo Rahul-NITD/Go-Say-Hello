5ddf9c7074c5ef04c12f7b0390b95db336f78b4c
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-17 10:11:47 +0000

    Added StackofInt

--- Generics/Stacks/stacks.go
+++ Generics/Stacks/stacks.go
@@ -1,1 +1,26 @@
 package stacks
+
+import "errors"
+
+type StackOfInt struct {
+	values []int
+}
+
+func (s *StackOfInt) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *StackOfInt) Push(x int) {
+	s.values = append(s.values, x)
+}
+
+func (s *StackOfInt) Pop() (int, error) {
+	if s.IsEmpty() {
+		return 0, errors.New("stack is empty")
+	}
+	idx := len(s.values) - 1
+	val := s.values[idx]
+	s.values = s.values[:idx]
+	return val, nil
+
+}
--- Generics/Stacks/stacks_test.go
+++ Generics/Stacks/stacks_test.go
@@ -1,1 +1,26 @@
 package stacks_test
+
+import (
+	stacks "GoSayHello/Generics/Stacks"
+	"testing"
+)
+
+func AssertEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v != %v", got, want)
+	}
+}
+
+func TestStack(t *testing.T) {
+	t.Run("For Integers", func(t *testing.T) {
+		st := new(stacks.StackOfInt)
+		AssertEqual(t, st.IsEmpty(), true)
+		st.Push(5)
+		AssertEqual(t, st.IsEmpty(), false)
+		val, err := st.Pop()
+		AssertEqual(t, val, 5)
+		AssertEqual(t, err, nil)
+		AssertEqual(t, st.IsEmpty(), true)
+	})
+}
