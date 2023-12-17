284adb697653a8d2a49a801789630065bd462443
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-17 11:49:35 +0000

    Implemented Bank using Reduce functions

---
+++ Generics/ReduceFn/Bank/Bank.go
@@ -0,0 +1,25 @@
+package bank
+
+import reducefn "GoSayHello/Generics/ReduceFn"
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  int
+}
+
+func BalanceFor(txs []Transaction, name string) int {
+	return reducefn.Reduce(
+		txs,
+		func(resTillNow int, t Transaction) int {
+			if t.From == name {
+				return resTillNow - t.Sum
+			}
+			if t.To == name {
+				return resTillNow + t.Sum
+			}
+			return resTillNow
+		},
+		0,
+	)
+}
---
+++ Generics/ReduceFn/Bank/Bank_test.go
@@ -0,0 +1,33 @@
+package bank_test
+
+import (
+	bank "GoSayHello/Generics/ReduceFn/Bank"
+	"testing"
+)
+
+func AssertEqual[T comparable](t testing.TB, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v != %v", got, want)
+	}
+}
+
+func TestBank(t *testing.T) {
+	transactions := []bank.Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual[int](t, bank.BalanceFor(transactions, "Riya"), 100)
+	AssertEqual[int](t, bank.BalanceFor(transactions, "Chris"), -75)
+	AssertEqual[int](t, bank.BalanceFor(transactions, "Adil"), -25)
+
+}
