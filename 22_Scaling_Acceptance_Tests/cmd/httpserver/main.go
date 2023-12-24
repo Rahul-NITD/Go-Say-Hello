e74ef5877884eb4422bc041639534b5b7bd005dc
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-24 14:11:53 +0000

    iteration 0 done

--- 22_Scaling_Acceptance_Tests/cmd/httpserver/main.go
+++ 22_Scaling_Acceptance_Tests/cmd/httpserver/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
+	handler := http.HandlerFunc(Handler)
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
