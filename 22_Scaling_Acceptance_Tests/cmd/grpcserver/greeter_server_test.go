c032e5812b4501fbebe6922248fb33df7354f20c
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-24 19:34:52 +0000

    gRPC support

---
+++ 22_Scaling_Acceptance_Tests/cmd/grpcserver/greeter_server_test.go
@@ -0,0 +1,22 @@
+package main_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Rahul-NITD/scalingacceptancetests/adapters"
+	"github.com/Rahul-NITD/scalingacceptancetests/adapters/grpcserver"
+	"github.com/Rahul-NITD/scalingacceptancetests/specs"
+)
+
+func TestGreeterServer(t *testing.T) {
+
+	var (
+		port           = "50051"
+		dockerFilePath = "./cmd/grpcserver/DockerFile"
+		driver         = grpcserver.Driver{Addr: fmt.Sprintf("http://localhost:%s", port)}
+	)
+
+	adapters.StartDockerServer(t, port, dockerFilePath)
+	specs.GreeterSpecification(t, driver)
+}
