4ddfadf47fa25309d46b3c9cd0f56efb080e963d
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-24 18:43:19 +0000

    refoactored TestGreeterServer

--- 22_Scaling_Acceptance_Tests/cmd/httpserver/greeter_server_test.go
+++ 22_Scaling_Acceptance_Tests/cmd/httpserver/greeter_server_test.go
@@ -1,46 +1,22 @@
 package main_test
 
 import (
-	"context"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/Rahul-NITD/scalingacceptancetests/adapters"
 	"github.com/Rahul-NITD/scalingacceptancetests/adapters/httpserver"
 	"github.com/Rahul-NITD/scalingacceptancetests/specs"
-	"github.com/alecthomas/assert/v2"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestGreeterServer(t *testing.T) {
-	ctx := context.Background()
-
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context:    "../../.", // Verify that this path is correct and includes the Dockerfile
-			Dockerfile: "./cmd/httpserver/Dockerfile",
-		},
-		ExposedPorts: []string{"8080/tcp"},
-		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	}
-
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	assert.NoError(t, err)
-
-	// Clean up the container after the test
-	t.Cleanup(func() {
-		assert.NoError(t, container.Terminate(ctx))
-	})
-
-	// Get the container's host port
-	port, err := container.MappedPort(ctx, "8080")
-	assert.NoError(t, err)
-
-	// Use the host port to set the BaseURL
-	driver := httpserver.Driver{BaseURL: "http://localhost:" + port.Port(), Client: &http.Client{Timeout: 1 * time.Second}}
+	var (
+		port           = "8080"
+		DockerFilePath = "./cmd/httpserver/Dockerfile"
+		baseURL        = "http://localhost:" + port
+		driver         = httpserver.Driver{BaseURL: baseURL, Client: &http.Client{Timeout: 1 * time.Second}}
+	)
+	adapters.StartDockerServer(t, port, DockerFilePath)
 	specs.GreeterSpecification(t, driver)
 }
