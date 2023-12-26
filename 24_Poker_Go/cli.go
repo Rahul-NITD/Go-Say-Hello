13a911a75f4225b7487cb4ec95b3e1dff0d8c783
Rahul-NITD <211220042@nitdelhi.ac.in>
2023-12-26 15:18:32 +0530

    Added Game

--- 24_Poker_Go/cli.go
+++ 24_Poker_Go/cli.go
@@ -7,14 +7,12 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type CLI struct {
-	store   PokerStorage
-	inp     *bufio.Scanner
-	alerter BlindAlerter
-	out     io.Writer
+	inp  *bufio.Scanner
+	out  io.Writer
+	game Game
 }
 
 func (c *CLI) PlayPoker() {
@@ -31,32 +29,19 @@ func (c *CLI) PlayPoker() {
 	if err != nil {
 		log.Fatalf("Could not convert to integer. Run again %q", txt)
 	}
-	alertBlinds(c, players)
+	c.game.Start(players)
 
 	c.inp.Scan()
-	c.store.RecordWin(strings.Replace(c.inp.Text(), " wins", "", 1))
-}
-
-func alertBlinds(c *CLI, numberOfPlayers int) {
-	if c.alerter != nil {
-		blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
-
-		blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-		blindTime := 0 * time.Second
-		for _, blind := range blinds {
-			c.alerter.ScheduleAlertAfter(blindTime, blind)
-			blindTime = blindTime + blindIncrement
-		}
-	} else {
-		log.Println("No alerter passed")
-	}
+	c.game.Finish(strings.Replace(c.inp.Text(), " wins", "", 1))
 }
 
 func NewCLI(store PokerStorage, inp io.Reader, alerter BlindAlerter, out io.Writer) *CLI {
 	return &CLI{
-		store:   store,
-		inp:     bufio.NewScanner(inp),
-		alerter: alerter,
-		out:     out,
+		inp: bufio.NewScanner(inp),
+		out: out,
+		game: Game{
+			alerter: alerter,
+			store:   store,
+		},
 	}
 }
---
+++ 24_Poker_Go/game.go
@@ -0,0 +1,30 @@
+package poker
+
+import (
+	"log"
+	"time"
+)
+
+type Game struct {
+	alerter BlindAlerter
+	store   PokerStorage
+}
+
+func (g *Game) Start(numberOfPlayers int) {
+	if g.alerter != nil {
+		blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
+
+		blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+		blindTime := 0 * time.Second
+		for _, blind := range blinds {
+			g.alerter.ScheduleAlertAfter(blindTime, blind)
+			blindTime = blindTime + blindIncrement
+		}
+	} else {
+		log.Println("No alerter passed")
+	}
+}
+
+func (g *Game) Finish(winner string) {
+	g.store.RecordWin(winner)
+}
