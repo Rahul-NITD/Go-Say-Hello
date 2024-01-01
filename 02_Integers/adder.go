1ef80a179a33fce516d2b2d24b1e6de94dfba6e5
Rahul Goel <211220042@nitdelhi.ac.in>
2024-01-01 06:08:06 +0000

    minor changes to enable godoc

--- 02_Integers/adder.go
+++ 02_Integers/adder.go
@@ -1,6 +1,8 @@
+// This is the overview
 package numbergame
 
 // Takes 2 integer numbers and returns the sum of them
 func Adder(num1, num2 int) int {
 	return num1 + num2
 }
+
--- 02_Integers/adder_test.go
+++ 02_Integers/adder_test.go
@@ -1,20 +1,32 @@
-package numbergame
+package numbergame_test
 
-import "testing"
+// Overview?
+
+import (
+	numbergame "GoSayHello/02_Integers"
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	t.Run("test 2 + 2", func(t *testing.T) {
-		sum := Adder(2, 2)
+		sum := numbergame.Adder(2, 2)
 		expected := 4
 		if sum != expected {
 			t.Errorf("got '%d' but required '%d'", expected, sum)
 		}
 	})
 	t.Run("test 4+7", func(t *testing.T) {
-		sum := Adder(4, 7)
+		sum := numbergame.Adder(4, 7)
 		expected := 11
 		if sum != expected {
 			t.Errorf("got '%d' but required '%d'", expected, sum)
 		}
 	})
+}
+
+func ExampleAdder() {
+	sum := numbergame.Adder(3, 4)
+	fmt.Println(sum)
+	// Output: 7
 }
